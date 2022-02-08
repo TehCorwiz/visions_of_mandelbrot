8db917cddd733f1da50d75005b6d24a35065f403
Corwin Harris <git@corwin.betabeetle.com>
2022-02-08 11:47:39 -0500

    Split generating the mandelbrot set from rendering it. Converted logic to use an iterator.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 mod mandelbrot;
 
-use crate::mandelbrot::MandelbrotSet;
+use crate::mandelbrot::{MandelbrotGenerator, MandelbrotRenderer};
 use log::error;
 use pixels::{PixelsBuilder, SurfaceTexture};
 use std::rc::Rc;
@@ -18,19 +18,19 @@ const HEIGHT: u32 = 480;
 
 fn main() {
     #[cfg(target_arch = "wasm32")]
-    {
-        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        console_log::init_with_level(log::Level::Trace).expect("error initializing logger");
+        {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Trace).expect("error initializing logger");
 
-        wasm_bindgen_futures::spawn_local(run());
-    }
+            wasm_bindgen_futures::spawn_local(run());
+        }
 
     #[cfg(not(target_arch = "wasm32"))]
-    {
-        env_logger::init();
+        {
+            env_logger::init();
 
-        pollster::block_on(run());
-    }
+            pollster::block_on(run());
+        }
 }
 
 async fn run() {
@@ -47,47 +47,47 @@ async fn run() {
     let window = Rc::new(window);
 
     #[cfg(target_arch = "wasm32")]
-    {
-        use wasm_bindgen::JsCast;
-        use winit::platform::web::WindowExtWebSys;
+        {
+            use wasm_bindgen::JsCast;
+            use winit::platform::web::WindowExtWebSys;
 
-        // Retrieve current width and height dimensions of browser client window
-        let get_window_size = || {
+            // Retrieve current width and height dimensions of browser client window
+            let get_window_size = || {
+                let client_window = web_sys::window().unwrap();
+                LogicalSize::new(
+                    client_window.inner_width().unwrap().as_f64().unwrap(),
+                    client_window.inner_height().unwrap().as_f64().unwrap(),
+                )
+            };
+
+            let window = Rc::clone(&window);
+
+            // Initialize winit window with current dimensions of browser client
+            window.set_inner_size(get_window_size());
+
             let client_window = web_sys::window().unwrap();
-            LogicalSize::new(
-                client_window.inner_width().unwrap().as_f64().unwrap(),
-                client_window.inner_height().unwrap().as_f64().unwrap(),
-            )
-        };
 
-        let window = Rc::clone(&window);
+            // Attach winit canvas to body element
+            web_sys::window()
+                .and_then(|win| win.document())
+                .and_then(|doc| doc.body())
+                .and_then(|body| {
+                    body.append_child(&web_sys::Element::from(window.canvas()))
+                        .ok()
+                })
+                .expect("couldn't append canvas to document body");
 
-        // Initialize winit window with current dimensions of browser client
-        window.set_inner_size(get_window_size());
-
-        let client_window = web_sys::window().unwrap();
-
-        // Attach winit canvas to body element
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| doc.body())
-            .and_then(|body| {
-                body.append_child(&web_sys::Element::from(window.canvas()))
-                    .ok()
-            })
-            .expect("couldn't append canvas to document body");
-
-        // Listen for resize event on browser client. Adjust winit window dimensions
-        // on event trigger
-        let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
-            let size = get_window_size();
-            window.set_inner_size(size)
-        }) as Box<dyn FnMut(_)>);
-        client_window
-            .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
-            .unwrap();
-        closure.forget();
-    }
+            // Listen for resize event on browser client. Adjust winit window dimensions
+            // on event trigger
+            let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |_e: web_sys::Event| {
+                let size = get_window_size();
+                window.set_inner_size(size)
+            }) as Box<dyn FnMut(_)>);
+            client_window
+                .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+                .unwrap();
+            closure.forget();
+        }
 
     let mut input = WinitInputHelper::new();
     let mut pixels = {
@@ -100,12 +100,14 @@ async fn run() {
             .await
             .expect("Pixels error")
     };
-    let mut mandelbrot_set =  Box::new(MandelbrotSet::new(WIDTH as usize, HEIGHT as usize));
+
+    let mandelbrot_set = MandelbrotGenerator::new(WIDTH as usize, HEIGHT as usize, MandelbrotGenerator::DEFAULT_MAX_ITERATIONS);
+    let mut mandelbrot_renderer = MandelbrotRenderer::new(WIDTH as usize, HEIGHT as usize, mandelbrot_set);
 
     event_loop.run(move |event, _, control_flow| {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
-            mandelbrot_set.draw(pixels.get_frame());
+            mandelbrot_renderer.draw(pixels.get_frame());
             if pixels
                 .render()
                 .map_err(|e| error!("pixels.render() failed: {:?}", e))
@@ -127,20 +129,22 @@ async fn run() {
             // Zoom events
             if input.mouse_pressed(0) {
                 // Left mouse
-                mandelbrot_set.zoom(input.mouse().unwrap(), 0.5);
+                mandelbrot_renderer.zoom(input.mouse().unwrap(), 0.5);
             } else if input.mouse_pressed(1) {
                 // Right mouse
-                mandelbrot_set.zoom(input.mouse().unwrap(), 2.0);
+                mandelbrot_renderer.zoom(input.mouse().unwrap(), 2.0);
             }
 
             // Palette events
             if input.key_pressed(VirtualKeyCode::P) {
-                mandelbrot_set.randomize_palette();
+                mandelbrot_renderer.randomize_palette();
             }
 
             // Reset events
             if input.key_pressed(VirtualKeyCode::R) {
-                mandelbrot_set =  Box::new(MandelbrotSet::new(WIDTH as usize, HEIGHT as usize));
+                mandelbrot_renderer.resize(WIDTH as usize, HEIGHT as usize);
+                mandelbrot_renderer.generator = MandelbrotGenerator::new(WIDTH as usize, HEIGHT as usize, MandelbrotGenerator::DEFAULT_MAX_ITERATIONS);
+                mandelbrot_renderer.palette = MandelbrotRenderer::rainbow_palette(MandelbrotGenerator::DEFAULT_MAX_ITERATIONS as usize);
             }
 
             // Resize the window
@@ -148,11 +152,9 @@ async fn run() {
                 pixels.resize_surface(size.width, size.height);
                 pixels.resize_buffer(size.width, size.height);
 
-                mandelbrot_set.resize(size.width as usize, size.height as usize);
+                mandelbrot_renderer.resize(size.width as usize, size.height as usize);
             }
 
-            // Update internal state and request a redraw
-            mandelbrot_set.update();
             window.request_redraw();
         }
     });
--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,11 +1,11 @@
 use palette::{Gradient, LinSrgb};
 use rand::Rng;
 
-fn normalize(n: &f64, r_min: &f64, r_max: &f64, t_min: &f64, t_max: &f64) -> f64 {
+fn normalize(n: f64, r_min: f64, r_max: f64, t_min: f64, t_max: f64) -> f64 {
     (((n - r_min) / (r_max - r_min)) * (t_max - t_min)) + t_min
 }
 
-pub(crate) struct MandelbrotSet {
+pub(crate) struct MandelbrotGenerator {
     width: usize,
     height: usize,
     max_iterations: f64,
@@ -14,18 +14,16 @@ pub(crate) struct MandelbrotSet {
     y_scale_min: f64,
     y_scale_max: f64,
     iteration_counts: Vec<Vec<f64>>,
-    frame_buffer: Vec<u8>,
-    palette: Vec<LinSrgb>,
+    current_x: usize,
+    current_y: usize,
     recalculate: bool,
-    redraw: bool,
-    drawing: bool,
 }
 
-impl MandelbrotSet {
-    pub(crate) fn new(width: usize, height: usize) -> MandelbrotSet {
-        let max_iterations = 1000.0;
+impl MandelbrotGenerator {
+    pub const DEFAULT_MAX_ITERATIONS: f64 = 1000.0;
 
-        Self {
+    pub(crate) fn new(width: usize, height: usize, max_iterations: f64) -> MandelbrotGenerator {
+        MandelbrotGenerator {
             width,
             height,
             max_iterations,
@@ -34,22 +32,12 @@ impl MandelbrotSet {
             y_scale_min: -1.12,
             y_scale_max: 1.12,
             iteration_counts: vec![vec![0.0; width]; height],
-            frame_buffer: vec![0xff as u8; width * height * 4],
-            palette: MandelbrotSet::rainbow_palette(max_iterations as usize),
+            current_x: 0,
+            current_y: 0,
             recalculate: true,
-            redraw: true,
-            drawing: false,
         }
     }
 
-    pub(crate) fn update(&mut self) {
-        // TODO: Interactivity
-    }
-
-    pub(crate) fn randomize_palette(&mut self) {
-        self.palette = MandelbrotSet::random_palette(self.max_iterations as usize);
-        self.redraw = true;
-    }
 
     fn x_range(&self) -> f64 {
         (self.x_scale_max - self.x_scale_min).abs()
@@ -59,10 +47,14 @@ impl MandelbrotSet {
         (self.y_scale_max - self.y_scale_min).abs()
     }
 
-    // Adjusts the x and y scale values such that the scale of the image remains constant
-    //  regardless of resolution. This has the effect of expanding the canvas when increasing the
-    //  window size and zooming in when narrowing the window size.
-    // TODO: Consider adding the option to only adjust the aspect ratio.
+    pub fn resize(&mut self, width: usize, height: usize) {
+        self.resize_scaling_factors(width, height);
+        self.width = width;
+        self.height = height;
+        self.iteration_counts = vec![vec![0.0; width]; height];
+        self.recalculate();
+    }
+
     fn resize_scaling_factors(&mut self, width: usize, height: usize) {
         let x_ratio = width as f64 / self.width as f64;
         let y_ratio = height as f64 / self.height as f64;
@@ -80,16 +72,6 @@ impl MandelbrotSet {
         self.y_scale_max = self.y_scale_max + new_y_range_diff / 2.0;
     }
 
-    pub(crate) fn resize(&mut self, width: usize, height: usize) {
-        self.resize_scaling_factors(width, height);
-
-        self.width = width;
-        self.height = height;
-        self.frame_buffer = vec![0xff as u8; width * height * 4];
-        self.recalculate = true;
-        self.redraw = true;
-    }
-
     pub(crate) fn zoom(&mut self, coords: (f32, f32), factor: f64) {
         let x_range = self.x_range();
         let y_range = self.y_range();
@@ -98,18 +80,18 @@ impl MandelbrotSet {
         let new_y_range = y_range * factor;
 
         let new_midpoint_x = normalize(
-            &(coords.0 as f64),
-            &0.0,
-            &(self.width as f64),
-            &self.x_scale_min,
-            &self.x_scale_max,
+            coords.0 as f64,
+            0.0,
+            self.width as f64,
+            self.x_scale_min,
+            self.x_scale_max,
         );
         let new_midpoint_y = normalize(
-            &(coords.1 as f64),
-            &0.0,
-            &(self.height as f64),
-            &self.y_scale_min,
-            &self.y_scale_max,
+            coords.1 as f64,
+            0.0,
+            self.height as f64,
+            self.y_scale_min,
+            self.y_scale_max,
         );
 
         self.x_scale_min = new_midpoint_x - (new_x_range / 2.0);
@@ -118,40 +100,133 @@ impl MandelbrotSet {
         self.y_scale_min = new_midpoint_y - (new_y_range / 2.0);
         self.y_scale_max = new_midpoint_y + (new_y_range / 2.0);
 
-        self.recalculate = true;
-        self.redraw = true;
+        self.recalculate();
     }
 
-    /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
-    pub(crate) fn draw(&mut self, frame: &mut [u8]) {
-        self.drawing = true;
+    pub fn recalculate(&mut self) {
+        self.recalculate = true;
+    }
 
-        if self.recalculate {
-            self.recalculate = false;
+    fn test_pixel(&self, px: u32, py: u32) -> f64 {
+        let x0 = normalize(
+            px as f64,
+            0.0,
+            (self.width - 1) as f64,
+            self.x_scale_min,
+            self.x_scale_max,
+        );
 
-            // Counts the number of iterations in each pixel location.
-            self.iteration_counts = vec![vec![0.0; self.width]; self.height];
+        let y0 = normalize(
+            py as f64,
+            0.0,
+            (self.height - 1) as f64,
+            self.y_scale_min,
+            self.y_scale_max,
+        );
 
-            for (y, row) in self.iteration_counts.iter_mut().enumerate() {
-                for (x, val) in row.iter_mut().enumerate() {
-                    let x = x as u32;
-                    let y = y as u32;
+        let mut x: f64 = 0.0;
+        let mut y: f64 = 0.0;
+        let mut x2: f64 = 0.0;
+        let mut y2: f64 = 0.0;
 
-                    *val = MandelbrotSet::test_pixel(
-                        &x,
-                        &y,
-                        &self.width,
-                        &self.height,
-                        &self.max_iterations,
-                        &self.x_scale_min,
-                        &self.x_scale_max,
-                        &self.y_scale_min,
-                        &self.y_scale_max,
-                    );
-                }
+        let mut iteration = 0.0;
+
+        // Cardioid checking
+        let y0_2 = y0 * y0;
+        let p = ((x0 - 0.25).powf(2.0) + y0_2).sqrt();
+        if x0 <= p - 2.0 * p * p + 0.25 {
+            return self.max_iterations; // Large cardioid
+        } else if (x0 + 1.0).powf(2.0) + y0_2 <= 1.0 / 16.0 {
+            return self.max_iterations; // Period-2 bulb
+        }
+
+        let mut x_old = 0.0;
+        let mut y_old = 0.0;
+        let mut period = 0;
+
+        // Escape algorithm
+        while ((x2 + y2) <= 4.0) && iteration < self.max_iterations {
+            y = 2.0 * x * y + y0;
+            x = x2 - y2 + x0;
+            x2 = x * x;
+            y2 = y * y;
+
+            iteration += 1.0;
+
+            // Periodicity checking
+            if x == x_old && y == y_old {
+                return self.max_iterations;
+            }
+
+            period += 1;
+            if period > 20 {
+                period = 0;
+                x_old = x;
+                y_old = y;
             }
         }
 
+        if iteration < self.max_iterations {
+            let log_zn = (x2 + y2).log10();
+            let log_2 = 2.0_f64.log10();
+            let nu = (log_zn / log_2).log10() / log_2;
+            iteration = iteration + 1.0 - nu;
+        }
+
+        iteration
+    }
+}
+
+impl Iterator for MandelbrotGenerator {
+    type Item = f64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let x = self.current_x;
+        let y = self.current_y;
+
+        if self.recalculate {
+            self.iteration_counts[y][x] = self.test_pixel(x as u32, y as u32);
+        }
+
+        self.current_x += 1;
+
+        if self.current_x >= self.width {
+            self.current_x = 0;
+            self.current_y += 1;
+        }
+
+        if self.current_y >= self.height {
+            self.current_y = 0;
+            self.current_x = 0;
+            self.recalculate = false;
+        }
+
+        return Some(self.iteration_counts[y][x]);
+    }
+}
+
+pub(crate) struct MandelbrotRenderer {
+    pub(crate) generator: MandelbrotGenerator,
+    width: usize,
+    height: usize,
+    pub(crate) palette: Vec<LinSrgb>,
+    redraw: bool,
+    frame_buffer: Vec<u8>,
+}
+
+impl MandelbrotRenderer {
+    pub(crate) fn new(width: usize, height: usize, generator: MandelbrotGenerator) -> Self {
+        MandelbrotRenderer {
+            generator,
+            width,
+            height,
+            palette: MandelbrotRenderer::rainbow_palette(MandelbrotGenerator::DEFAULT_MAX_ITERATIONS as usize),
+            redraw: true,
+            frame_buffer: vec![0xff as u8; width * height * 4],
+        }
+    }
+
+    pub(crate) fn draw(&mut self, frame: &mut [u8]) {
         if self.redraw {
             self.redraw = false;
             self.draw_to_frame_buffer();
@@ -159,27 +234,22 @@ impl MandelbrotSet {
 
         frame.copy_from_slice(&self.frame_buffer);
 
-        self.drawing = false;
+        self.redraw = false;
     }
 
-    // The problem I'm going to have is that at higher zoom levels the x and y scale values will get
-    //  too small. I'll effectively hit the float resolution limit. Maybe use a library to provide
-    //  arbitrary resolution numbers?
     fn draw_to_frame_buffer(&mut self) {
-        for (i, pixel) in self.frame_buffer.chunks_exact_mut(4).enumerate() {
-            let x = i % self.width as usize;
-            let y = i / self.width as usize;
-
-            let rgba: [u8; 4] = if self.iteration_counts[y][x] == self.max_iterations {
+        for pixel in self.frame_buffer.chunks_exact_mut(4) {
+            let mandelbrot_value = self.generator.next().unwrap();
+            let rgba: [u8; 4] = if mandelbrot_value == self.generator.max_iterations {
                 [0, 0, 0, 0xff]
             } else {
-                let iterations: usize = self.iteration_counts[y][x].floor() as usize;
-                let fraction = self.iteration_counts[y][x] % 1.0;
+                let iterations: usize = mandelbrot_value.floor() as usize;
+                let fraction = mandelbrot_value % 1.0;
 
                 let color1 = self.palette[iterations];
                 let color2 = self.palette[iterations + 1];
 
-                MandelbrotSet::color_to_rgba(&Gradient::from([
+                MandelbrotRenderer::color_to_rgba(&Gradient::from([
                     (0.0, color1),
                     (1.0, color2)
                 ]).get(fraction as f32))
@@ -189,7 +259,25 @@ impl MandelbrotSet {
         }
     }
 
-    fn random_palette(n_colors: usize) -> Vec<LinSrgb> {
+    pub(crate) fn zoom(&mut self, coords: (f32, f32), factor: f64) {
+        self.generator.zoom(coords, factor);
+        self.redraw = true;
+    }
+
+    pub(crate) fn resize(&mut self, width: usize, height: usize) {
+        self.width = width;
+        self.height = height;
+        self.frame_buffer = vec![0xff as u8; width * height * 4];
+        self.generator.resize(width, height);
+        self.redraw = true;
+    }
+
+    pub(crate) fn randomize_palette(&mut self) {
+        self.palette = MandelbrotRenderer::random_palette(self.generator.max_iterations as usize);
+        self.redraw = true;
+    }
+
+    pub(crate) fn random_palette(n_colors: usize) -> Vec<LinSrgb> {
         let mut rng = rand::thread_rng();
         let mut pool: Vec<f32> = vec![0.0; 15];
         for i in 1..15 {
@@ -206,7 +294,7 @@ impl MandelbrotSet {
         ]).take(n_colors).collect()
     }
 
-    fn rainbow_palette(n_colors: usize) -> Vec<LinSrgb> {
+    pub(crate) fn rainbow_palette(n_colors: usize) -> Vec<LinSrgb> {
         Gradient::from(vec![
             (0.0, LinSrgb::new(1.0, 0.0, 0.0)),
             (0.05, LinSrgb::new(0.0, 1.0, 0.0)),
@@ -223,85 +311,5 @@ impl MandelbrotSet {
             (color.blue * 0xff as f32) as u8,
             0xff,
         ]
-    }
-
-    // Returns the number of iterations to diverge.
-    fn test_pixel(
-        px: &u32,
-        py: &u32,
-        width: &usize,
-        height: &usize,
-        max_iterations: &f64,
-        x_scale_min: &f64,
-        x_scale_max: &f64,
-        y_scale_min: &f64,
-        y_scale_max: &f64,
-    ) -> f64 {
-        let x0 = normalize(
-            &(*px as f64),
-            &0.0,
-            &((width - 1) as f64),
-            x_scale_min,
-            x_scale_max,
-        );
-
-        let y0 = normalize(
-            &(*py as f64),
-            &0.0,
-            &((height - 1) as f64),
-            y_scale_min,
-            y_scale_max,
-        );
-
-        let mut x: f64 = 0.0;
-        let mut y: f64 = 0.0;
-        let mut x2: f64 = 0.0;
-        let mut y2: f64 = 0.0;
-
-        let mut iteration = 0.0;
-
-        // Cardioid checking
-        let y0_2 = y0 * y0;
-        let p = ((x0 - 0.25).powf(2.0) + y0_2).sqrt();
-        if x0 <= p - 2.0 * p * p + 0.25 {
-            return *max_iterations; // Large cardioid
-        } else if (x0 + 1.0).powf(2.0) + y0_2 <= 1.0 / 16.0 {
-            return *max_iterations; // Period-2 bulb
-        }
-
-        let mut x_old = 0.0;
-        let mut y_old = 0.0;
-        let mut period = 0;
-
-        // Escape algorithm
-        while ((x2 + y2) <= 4.0) && iteration < *max_iterations {
-            y = 2.0 * x * y + y0;
-            x = x2 - y2 + x0;
-            x2 = x * x;
-            y2 = y * y;
-
-            iteration += 1.0;
-
-            // Periodicity checking
-            if x == x_old && y == y_old {
-                return *max_iterations;
-            }
-
-            period += 1;
-            if period > 20 {
-                period = 0;
-                x_old = x;
-                y_old = y;
-            }
-        }
-
-        if iteration < *max_iterations {
-            let log_zn = (x2 + y2).log10();
-            let log_2 = 2.0_f64.log10();
-            let nu = (log_zn / log_2).log10() / log_2;
-            iteration = iteration + 1.0 - nu;
-        }
-
-        iteration
     }
 }
